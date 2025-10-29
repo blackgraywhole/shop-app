c2d01d7e057af8c1e54ea2a7b7719656e321cda2
Airopetyants Artyom <s1nglolkek@mail.ru>
2025-10-29 23:09:36 +0300

    initial commit

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import uvicorn
+from routers import categories, products
+
+
+app = FastAPI(
+    title="FastAPI Интернет-магазин",
+    version="0.1.0",
+)
+
+app.include_router(categories.router)
+app.include_router(products.router)
+
+
+@app.get("/")
+async def root():
+    """
+    Корневой маршрут, подтверждающий, что API работает.
+    """
+    return {"message": "Добро пожаловать в API интернет-магазина!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
